660a2385b4a6b30a7667f944024f33a356f52681
Mona0509 <ktc25a31a0056@edu.kyoto-tech.ac.jp>
2026-06-21 01:51:39 +0900

    当たり判定部分完成　弾幕の生成未完成

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,12 +1,29 @@
+using Cysharp.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.U2D;
+using static UnityEngine.CullingGroup;
+
+public enum State
+{
+    Red, Blue
+}
 
 public class PlayerMove : MonoBehaviour
 {
     Vector2 mousePos;
     Vector2 worldPos;
-    void Start()
+
+    int playerHP = 3;
+    bool coolDownTime = false;
+    SpriteRenderer sprite;
+
+    State state;
+    private void Start()
     {
-        
+        sprite = GetComponent<SpriteRenderer>();
+        state = State.Red;
+        sprite.color = Color.red;
     }
 
     void Update()
@@ -16,6 +33,73 @@ public class PlayerMove : MonoBehaviour
         //transform.position = new Mathf.Clamp(worldPos,9.33f,4.86f);
         transform.position = worldPos;
 
-        Debug.Log(worldPos);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            state = StateChenge();
+        }
+    }
+
+    public State StateChenge()
+    {
+        switch (state)
+        {
+            case State.Red:
+                sprite.color = Color.blue;
+                return State.Blue;
+            case State.Blue:
+                sprite.color = Color.red;
+                return State.Red;
+        }
+        return state;
+    }
+
+    public async UniTask OnBulle(string tagName)
+    {
+        if (tagName == "Red" && state == State.Blue)
+        {
+            Color keepSprite = sprite.color;
+            PlayerHit();
+            await UniTask.Delay(2500);
+            sprite.color = keepSprite;
+            coolDownTime = false;
+        }
+        else if(tagName == "Blue" && state == State.Red)
+        {
+            Color keepSprite = sprite.color;
+            PlayerHit();
+            await UniTask.Delay(2500);
+            sprite.color = keepSprite;
+            coolDownTime = false;
+        }
+        else
+        {
+            await UniTask.Delay(0000);
+        }
+    }
+
+
+    public void PlayerHit()
+    {
+        coolDownTime = true;
+        sprite.color = Color.gray;
+        --playerHP;
+        if(playerHP <= 0)
+        {
+            Debug.Log("GameOver");
+        }
+    }
+
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("G‚ê‚½");
+        if (coolDownTime) return;
+        Debug.Log("ƒŠƒ^[ƒ“‚ð’´‚¦‚½");
+        if (collision.CompareTag("Red") || collision.CompareTag("Blue"))
+        {
+            string tagName = collision.tag;
+            OnBulle(tagName).Forget();
+        }
     }
 }
